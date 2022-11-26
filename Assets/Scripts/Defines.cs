e27986aece1e58e038214b0d5ce76cdcdf0130a4
watanabe@marida <watanabe.everystudio@gmail.com>
2022-11-26 22:04:59 +0900

    ダイアログ用のスクリプタブルオブジェクト作ったり Dialogue Scriptable Objects

---
+++ Assets/Scripts/Defines.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Defines
+{
+    public const string ProjectMenuName = "RPGプロジェクト";
+}
---
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "新しいDialogue", menuName = Defines.ProjectMenuName + "/Dialogue")]
+public class Dialogue : ScriptableObject
+{
+    [SerializeField] private DialogueNode[] nodes;
+
+
+}
